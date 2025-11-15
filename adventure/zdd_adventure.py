7a27b745917ccdbb05f9545e4a7250137f6f12c9
Alexandra Molsberger <alexandra.molsberger@study.hs-duesseldorf.de>
2025-11-15 12:45:41 +0100

    added cooling fan

--- adventure/zdd_adventure.py
+++ adventure/zdd_adventure.py
@@ -34,7 +34,8 @@ class ZDDAdventure:
 
         # Define rooms in each floor
         analog_book = Item("old book", "a real book made of paper", movable=True)
-        archive_room = Room("archive", "Old records and dusty books everywhere.", analog_book)
+        cooling_fan = Item("cooling fan", "a portable USB cooling fan", movable=True)
+        archive_room = Room("archive", "Old records and dusty books everywhere.", [analog_book, cooling_fan])
         cellar.add_room("archive", archive_room)
         cellar.add_room("toilet", ALL_ROOMS["toilet_cellar"])
 
