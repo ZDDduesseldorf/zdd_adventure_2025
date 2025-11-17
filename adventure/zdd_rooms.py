73505425cfa4f94733c75195d4960354bdb9384a
sahra <sahra.ogul@study.hs-duesseldorf.de>
2025-11-17 16:18:20 +0100

    issue 66"Room"

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -1,5 +1,6 @@
 """This is to keep all special rooms of the ZDD."""
-from main_classes import Room 
+from main_classes import Room, Item
+
 
 class ToiletCellar(Room):
     def run_story(self, user_items):
@@ -16,40 +17,38 @@ class YourRoom(Room):
     def run_story(self, user_items):
         print("You enter your room. It's calm... but something feels off.")
 
-        
         if "laptop" in [x.name for x in user_items]:
             print("Your laptop suddenly lights up in your backpack.")
             print("A huge wall of unfinished tasks appears on the screen.")
             print("The stress hits you instantly... it's overwhelming.")
 
-          
             action = input("Do you want to smash the laptop out of frustration? (yes/no): ").strip().lower()
 
             if action == "yes":
                 print("You slam the laptop on the ground. The screen shatters into pieces.")
                 print("Strangely enough... you feel a bit better.")
-                
                 return [x for x in user_items if x.name != "laptop"]
 
             print("You hold yourself back and close the laptop.")
             print("You're stressed... but proud of your self-control.")
+            return user_items
 
         else:
             print("You look around. Without your laptop, the room feels strangely quiet.")
-        
-        return user_items
+            return user_items
 
-    
+
 # -----------------------------------------------------------
 # ------------------- List here all rooms -------------------
-toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
-your_room= YourRoom("your Personal Room", "This is your Personal space ...you are not alone work is waiting for u")
-# -----------------------------------------------------------
-# Add YOUR ROOM instance here, similar to the example below:
-# my_room = MyRoom("room_name", "room_description")
 
+toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
+
+your_room = YourRoom(
+    "your_room",
+    "This is your personal space ... you are not alone, work is waiting for you."
+)
 
 ALL_ROOMS = {
     "toilet_cellar": toilet_cellar,
-    
+    "your_room": your_room,
 }
