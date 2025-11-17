bc1c5376a3607cf58b5a6a48f84429dfe549e760
sahra <sahra.ogul@study.hs-duesseldorf.de>
2025-11-17 15:37:31 +0100

    Added Room "your Room"

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -1,6 +1,5 @@
 """This is to keep all special rooms of the ZDD."""
-from main_classes import Room, Item
-
+from main_classes import Room 
 
 class ToiletCellar(Room):
     def run_story(self, user_items):
@@ -13,36 +12,38 @@ class ToiletCellar(Room):
             return [x for x in user_items if x.name != "old book"]
         return user_items
 
-class your_room (Room):
-    def run_story (self,user_items): 
-        print ("you're now in your room and see ur laptop on the table")
-        print("The laptop screen is still glowing faintly... maybe you should inspect it.")
+class YourRoom(Room):
+    def run_story(self, user_items):
+        print("You enter your room. It's calm... but something feels off.")
 
-        action = input("Type ""Inspect"" to check the Laptop or Press Enter to ignore it.").strip().lower()
-        if action == "inspect":
-            print("You open the laptop. Oh no...")
-            print("A list of unfinished homework pops up!")
-            print("ZDD Adventure Project -Tasks to complete:")
-            print("1) Implement YourRoom story")
-            print("2) Add task list interactions")
-            print("3) Don't forget to commit & push your work!")
+        
+        if "laptop" in [x.name for x in user_items]:
+            print("Your laptop suddenly lights up in your backpack.")
+            print("A huge wall of unfinished tasks appears on the screen.")
+            print("The stress hits you instantly... it's overwhelming.")
 
-            print("\nThe overwhelming list makes you feel extremely stressed and tense...")
-            print("U slowley feel the urge to smash the Laptop to the ground..")
-            action2 = input ("If u want to break it type (yes/no)").strip().lower()
-            if action2== ("yes"):
-                 print("In a sudden burst of frustration, you slam the laptop shut so hard the screen cracks.")
-                 print("The laptop is now completely broken... but weirdly, you feel a bit better.")
-            else : 
-                print("You take a deep breath and decide NOT to break the laptop.")
-                print("you're such a calm Person maybe next time, now do ur work than...")
+          
+            action = input("Do you want to smash the laptop out of frustration? (yes/no): ").strip().lower()
 
+            if action == "yes":
+                print("You slam the laptop on the ground. The screen shatters into pieces.")
+                print("Strangely enough... you feel a bit better.")
+                
+                return [x for x in user_items if x.name != "laptop"]
+
+            print("You hold yourself back and close the laptop.")
+            print("You're stressed... but proud of your self-control.")
+
+        else:
+            print("You look around. Without your laptop, the room feels strangely quiet.")
+        
         return user_items
+
     
 # -----------------------------------------------------------
 # ------------------- List here all rooms -------------------
 toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
-your_room= your_room("your Personal Room", "This is your Personal space ...you are not alone work is waiting for u")
+your_room= YourRoom("your Personal Room", "This is your Personal space ...you are not alone work is waiting for u")
 # -----------------------------------------------------------
 # Add YOUR ROOM instance here, similar to the example below:
 # my_room = MyRoom("room_name", "room_description")
@@ -50,5 +51,5 @@ your_room= your_room("your Personal Room", "This is your Personal space ...you a
 
 ALL_ROOMS = {
     "toilet_cellar": toilet_cellar,
-    "your_room": your_room,
+    
 }
