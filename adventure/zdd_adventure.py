b48e014d1c22b703e2453031d514c6bd36f94494
Linnea Brehe <linnea.brehe@study.hs-duesseldorf.de>
2025-11-17 15:04:26 +0100

    set movable to true for nintendo switch

--- adventure/zdd_adventure.py
+++ adventure/zdd_adventure.py
@@ -34,7 +34,7 @@ class ZDDAdventure:
 
         # Define rooms in each floor
         analog_book = Item("old book", "a real book made of paper", movable=True)
-        nintendo_switch = Item("Nintendo Switch", "A gaming console. Playing on a big TV screen would be nice...")
+        nintendo_switch = Item("Nintendo Switch", "A gaming console. Playing on a big TV screen would be nice...", movable=True)
         storeroom = Room("storeroom", "a small storage room with old computer stuff in it.", nintendo_switch)
         archive_room = Room("archive", "Old records and dusty books everywhere.", analog_book)
         cellar.add_room("archive", archive_room)
