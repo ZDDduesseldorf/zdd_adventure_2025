be0affa1b043eed8c6aaa9e8a0d94475f735f754
Yaman Kisanieh <yaman.kisanieh@study.hs-duesseldorf.de>
2025-11-18 23:58:15 +0100

    Add laptop item to cellar room

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -1,29 +1,44 @@
 """This is to keep all special rooms of the ZDD."""
-from main_classes import Room
+from main_classes import Item, Room
 
 
 class ToiletCellar(Room):
     def run_story(self, user_items):
         print("What did you expect? It's a toilet.")
-        # Check by name; if the book is present, drop it (story event)
         if "old book" in [x.name for x in user_items]:
-            print("While you wash your hands, the book slips out of your backpack ...right into the water.")
+            print(
+                "While you wash your hands, the book slips out of your backpack "
+                "...right into the water."
+            )
             print("You decide that it wasn't that important after all.")
-            # Remove book from inventory
             return [x for x in user_items if x.name != "old book"]
         return user_items
 
 
 # -----------------------------------------------------------
-# ------------------- List here all rooms -------------------
-toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
+# ------------------- New item: Laptop ----------------------
+# -----------------------------------------------------------
+
+laptop = Item(
+    name="laptop",
+    description=(
+        "A laptop sitting on the table, used to build the ZDDAdventure project. "
+        "Interacting with the laptop shows the homework you are supposed to do "
+        "for the ZDDAdventure project."
+    ),
+    movable=False,
+)
 
 # -----------------------------------------------------------
-# Add YOUR ROOM instance here, similar to the example below:
-# my_room = MyRoom("room_name", "room_description")
+# ------------------- List here all rooms -------------------
+# -----------------------------------------------------------
+
+toilet_cellar = ToiletCellar(
+    "toilet",
+    "Yes, even the cellar has a toilet.",
+    items_init=[laptop],
+)
 
 ALL_ROOMS = {
     "toilet_cellar": toilet_cellar
-    # Add your room key-value pairs here:
-    # "my_room_key": my_room
 }
